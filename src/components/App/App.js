c3f17e3f3f8e2bce94d2a6bc48f41af91cd9c532
void4d <tulubaevp@icloud.com>
2023-10-08 02:29:50 +0500

    fix: Исправить main на promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from '../Header/Header.js';
-import Main from '../Main/Main.js';
+import Promo from '../Promo/Promo.js';
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <Main />
+      <Promo />
     </div>
   );
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,20 @@
+import landingLogo from '../../images/landing-logo.svg'
+
+function Promo() {
+  return (
+    <main className="promo">
+      <div className="promo__block">
+        <div className="promo__text">
+          <h1 className="promo__title">Учебный проект студента факультета Веб&#x2011;разработки.</h1>
+          <p className="promo__more">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <div className="promo__more-button">
+            <div className="promo__more-button-text">Узнать больше</div>
+          </div>
+        </div>
+        <img className="promo__landing-logo" src={landingLogo} alt='Веб'></img>
+      </div>
+    </main>
+  );
+}
+
+export default Promo;
