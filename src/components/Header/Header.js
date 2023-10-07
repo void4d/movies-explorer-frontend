70cb71ed9b8925e71a52ccbd6e262236abbbe0eb
void4d <tulubaevp@icloud.com>
2023-10-07 23:26:09 +0500

    Добавить блок неавторизованного пользователя

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,33 @@
 import logo from '../../images/logo.svg';
 
 function Header() {
+  const isLoggedIn = true;
+
+  const loggedInBlock = (
+    <div className="header__account-block">
+      <div className="header__movies">Фильмы</div>
+      <div className="header__saved-movies">Сохранённые фильмы</div>
+      <div className="header__account">Аккаунт</div>
+      <div className="header__account-icon-container">
+        <div className="header__account-icon" alt="Аккаунт"></div>
+      </div>
+    </div>
+  );
+
+  const loggedOutBlock = (
+    <div className="header__account-block">
+      <div className="header__register-button">Регистрация</div>
+      <div className="header__login-button">
+        <div className="header__login-text">Войти</div>
+      </div>
+    </div>
+  );
+
   return (
     <header className="header">
       <div className="header__block">
         <img className="header__logo" src={logo} alt="Логотип"></img>
-        <div className="header__account-block">
-          <div className="header__movies">Фильмы</div>
-          <div className="header__saved-movies">Сохранённые фильмы</div>
-          <div className="header__account">Аккаунт</div>
-          <div className="header__account-icon-container">
-            <div className="header__account-icon" alt="Аккаунт"></div>
-          </div>
-        </div>
+        {isLoggedIn ? loggedInBlock : loggedOutBlock}
       </div>
     </header>
   );
