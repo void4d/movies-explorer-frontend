8266abebf4abe0de9abaf015b02617f0e6fe595e
void4d <tulubaevp@icloud.com>
2023-10-08 00:42:56 +0500

    add: Добавить верхний блок

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../Header/Header.js';
+import Main from '../Main/Main.js';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Main />
     </div>
   );
 }
