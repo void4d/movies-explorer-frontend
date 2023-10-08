21ab6856c581ba9c296faf9da3c53d92abc1fbb8
void4d <tulubaevp@icloud.com>
2023-10-09 04:00:43 +0500

    add: Добавить блок Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header.js';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className='app'>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
