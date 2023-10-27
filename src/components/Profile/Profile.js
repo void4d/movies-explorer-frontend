efd77f252a840b1fd4f8329b8cbf2d3394fddabb
void4d <tulubaevp@icloud.com>
2023-10-28 00:05:21 +0500

    fix: Исправить окно редактирования профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,17 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Profile() {
+  const inputRef = useRef(null);
   const [isEditing, setIsEditing] = useState(false);
+  const name = 'Павел';
+  const mail = 'pochta@mail.ru';
 
   function handleEditClick() {
     setIsEditing(!isEditing);
   }
 
-  const name = 'Павел';
-  const mail = 'pochta@mail.ru';
+  useEffect(() => {
+    if (isEditing) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
 
-  const saveProfileButton = (
+  const editProfileData = (
     <>
+      <div className="profile__data">
+        <div className="profile__username-container">
+          <div className="profile__username">Имя</div>
+          <input ref={inputRef} className="profile__username input" placeholder="Введите имя"></input>
+        </div>
+        <div className="profile__email-container">
+          <div className="profile__email">E-mail</div>
+          <input className="profile__email input" placeholder="Введите E-mail"></input>
+        </div>
+      </div>
       <span className="profile__error-message profile__error-message_hidden">
         При обновлении профиля произошла ошибка
       </span>
@@ -21,8 +37,18 @@ function Profile() {
     </>
   );
 
-  const EditAndLogOutButton = (
+  const profileData = (
     <>
+      <div className="profile__data">
+        <div className="profile__username-container">
+          <div className="profile__username">Имя</div>
+          <div className="profile__username">{name}</div>
+        </div>
+        <div className="profile__email-container">
+          <div className="profile__email">E-mail</div>
+          <div className="profile__email">{mail}</div>
+        </div>
+      </div>
       <button className="profile__edit-button" onClick={handleEditClick}>
         Редактировать
       </button>
@@ -35,17 +61,7 @@ function Profile() {
       <section className="profile">
         <div className="profile__container">
           <h2 className="profile__greeting">Привет, {name}!</h2>
-          <div className="profile__data">
-            <div className="profile__username-container">
-              <div className="profile__username">Имя</div>
-              <div className="profile__username">{name}</div>
-            </div>
-            <div className="profile__email-container">
-              <div className="profile__email">E-mail</div>
-              <div className="profile__email">{mail}</div>
-            </div>
-          </div>
-          {isEditing ? saveProfileButton : EditAndLogOutButton}
+          {isEditing ? editProfileData : profileData}
         </div>
       </section>
     </main>
