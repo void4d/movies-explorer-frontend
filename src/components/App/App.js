4b4045d6cae8b8797615e2794107648bbe5c4f8d
void4d <tulubaevp@icloud.com>
2023-10-28 21:27:29 +0500

    add: Добавить компонент ProtectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,7 @@ import Profile from '../Profile/Profile';
 import { Routes, Route } from 'react-router-dom';
 import SideMenu from '../SideMenu/SideMenu';
 import { useState } from 'react';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 function App() {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
@@ -32,9 +33,9 @@ function App() {
       <Header isLoggedIn={isLoggedIn} onClick={openSideMenu} />
       <Routes>
         <Route path="/" element={<Main isLoggedIn={isLoggedIn} />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/saved-movies" element={<SavedMovies />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/movies" element={<ProtectedRoute component={Movies} isLoggedIn={isLoggedIn} />} />
+        <Route path="/saved-movies" element={<ProtectedRoute component={SavedMovies} isLoggedIn={isLoggedIn} />} />
+        <Route path="/profile" element={<ProtectedRoute component={Profile} isLoggedIn={isLoggedIn} />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/signin" element={<Login />} />
         <Route path="*" element={<NotFoundPage />}></Route>
